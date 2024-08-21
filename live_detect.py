20c7e558061347e03dddc2566db7df82cc1b7c04
sandy-coder02 <104342827+sandy-coder02@users.noreply.github.com>
2024-08-21 18:08:17 +0530

    Add files via upload

--- live_detect.py
+++ live_detect.py
@@ -3,7 +3,7 @@ import cv2
 
 #Load the Cascade Classifier File
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
-
+ 
 # Define a video capture object
 vid = cv2.VideoCapture(0)
 
